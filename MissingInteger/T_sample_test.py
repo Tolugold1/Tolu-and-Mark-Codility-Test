597ef2917484b88e695cb7988d22dba90be50aff
Tolugold <toluadelekesteve@gmail.com>
2023-03-28 18:33:07 +0100

    First Solution

--- MissingInteger/T_sample_test.py
+++ MissingInteger/T_sample_test.py
@@ -1,11 +1,7 @@
 def solution(A: list)-> int:
     """Return smallest positive number greater than 0 and not in A"""
-    for i in range(len(A)):
-        if A[i] < 0:
-            min_num = "lt-0"
-        else:
-            min_num = 'gt-0'
-    if min_num == 'gt-0':
+    max_num = max(A)
+    if max_num > 0:
         intergers_not_in_A = [i for i in range(1, 100001) if i not in A]
         result = min(intergers_not_in_A)
         return result
